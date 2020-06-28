a1fe193c5a7096fd6c2eddb98084b126c62d69cb
Sanjib Dhar <sanjib@r-cis.com>
2020-06-28 19:09:25 +0600

    react app

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Title from "./title/title"
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8">
+            <Title />
+          </div>
+          <div className="col-lg-4">sefoijef</div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title/title.js
@@ -0,0 +1,95 @@
+import React, {Component} from "react";
+import './title.css'
+
+
+class Title extends Component{
+
+constructor(props){
+    super(props);
+
+    this.state = {
+        title : "My Title",
+        Isinput: false
+    };
+}
+
+editHandler(){
+    this.setState({
+        ...this.state,
+        Isinput:true
+    })
+}
+
+inputChange(event){
+    this.setState({
+        ...this.state,
+        title : event.target.value
+    })
+}
+
+keyPressHandler(event){
+    if(event.key == 'Enter'){
+        this.setState({
+            ...this.state,
+            Isinput:false
+        })
+    }
+}
+
+blurHandler(){
+    this.setState({
+        ...this.state,
+        Isinput:false
+    })
+}
+
+//  counter(){
+//      this.setState({
+//          number: this.state.number + 1
+//      })
+//  }
+
+
+    render(){
+        let output = null
+
+        if(this.state.Isinput){
+            output = (
+                <div>
+                    <h1 className='display-3'></h1>
+                    <input type="text" className="form-control" id="inputPassword2" 
+                    onChange={(event)=>{ this.inputChange(event)}} 
+                    onKeyPress = {(event)=> {this.keyPressHandler(event)}}
+                    onBlur ={()=> {this.blurHandler()}}
+                    value={this.state.title} placeholder="title name" />
+                </div>
+            )
+        }else{
+            output = (
+                <div className="d-flex title">
+                    <h1 className='display-3'>{this.state.title}</h1>
+                    <samp onClick= {()=>this.editHandler()} className='ml-auto mt-4 edit-btn'>
+                        <button type="submit" className="btn btn-primary mb-2">Confirm identity</button>
+                    </samp>
+                </div>
+            )
+            
+        }
+
+        return (
+            <div>
+                {output}
+            </div>
+        )
+    }
+}
+
+export default Title
+
+
+{/* <form className="form-inline">
+                        <div className="form-group mx-sm-3 mb-2">
+                            <input type="text" className="form-control" id="inputPassword2" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-primary mb-2">Confirm identity</button>
+                    </form> */}
